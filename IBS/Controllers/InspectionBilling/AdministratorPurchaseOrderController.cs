72b148ed73ce4b59514fc2f33acddbaaa2f67063
HardikGujjarSTTL <hardik.gujjar@silvertouch.com>
2023-08-25 16:38:20 +0530

    for Admi PO

---
+++ IBS/Controllers/InspectionBilling/AdministratorPurchaseOrderController.cs
@@ -0,0 +1,35 @@
+﻿using IBS.Filters;
+using IBS.Interfaces;
+using IBS.Interfaces.Inspection_Billing;
+using IBS.Models;
+using IBS.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBS.Controllers.InspectionBilling
+{
+    [Authorization]
+    public class AdministratorPurchaseOrderController : BaseController
+    {
+        #region Variables
+        private readonly IAdministratorPurchaseOrderRepository pIAdministratorPurchaseOrderRepository;
+        #endregion
+        public AdministratorPurchaseOrderController(IAdministratorPurchaseOrderRepository _pIAdministratorPurchaseOrderRepository)
+        {
+            pIAdministratorPurchaseOrderRepository = _pIAdministratorPurchaseOrderRepository;
+        }
+
+        [Authorization("AdministratorPurchaseOrder", "Index", "view")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LoadTable([FromBody] DTParameters dtParameters)
+        {
+            string region_code = Convert.ToString(IBS.Helper.SessionHelper.UserModelDTO.Region);
+            DTResult<AdministratorPurchaseOrderModel> dTResult = pIAdministratorPurchaseOrderRepository.GetPOMasterList(dtParameters, region_code);
+            return Json(dTResult);
+        }
+    }
+}
---
+++ IBS/Interfaces/Inspection&Billing/IAdministratorPurchaseOrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using IBS.Models;
+
+namespace IBS.Interfaces.Inspection_Billing
+{
+    public interface IAdministratorPurchaseOrderRepository
+    {
+        public PO_MasterModel FindByID(string CaseNo);
+        DTResult<AdministratorPurchaseOrderModel> GetPOMasterList(DTParameters dtParameters,string region_code);
+        bool Remove(string CaseNo, int UserID);
+        string POMasterDetailsInsertUpdate(PO_MasterModel model);
+        public PO_MasterModel FindCaseNo(string CaseNo,int VendCd);
+        DTResult<PO_MasterDetailListModel> GetPOMasterDetailsList(DTParameters dtParameters);
+        bool RemovePODetails(string CaseNo,string ITEM_SRNO, int UserID);
+        public int GenerateITEM_SRNO(string CASE_NO);
+        public PO_MasterDetailsModel FindPODetailsByID(string CASE_NO, string ITEM_SRNO);
+        DTResult<PO_MasterDetailsModel> FindByUOMDetail(decimal id);
+        int POMasterSubDetailsInsertUpdate(PO_MasterDetailsModel model);
+        string UpdateRealCaseNo(DEOVendorPurchesOrderModel model);
+        string getVendorEmail(string CASE_NO);
+
+        string[] GenerateRealCaseNo(string REGION_CD, string CASE_NO, string USER_ID);
+    }
+}
