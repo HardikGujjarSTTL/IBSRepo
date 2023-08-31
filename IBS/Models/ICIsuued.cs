d74b2bdb907c1656d8a9f312228ac9cbc2766c7f
23080102 <23080102@DESKTOP-FK9USVM>
2023-08-31 15:12:30 +0530

    cm wise ic issued page

--- IBS/Models/ICIsuued.cs
+++ IBS/Models/ICIsuued.cs
@@ -7,7 +7,8 @@
         public string SET_NO { get; set; }
         public string IE_NAME { get; set; }
         public string CO_NAME { get; set; }
-        public string FromDT { get; set; }
-        public string ToDT { get; set; }
+        public DateTime FromDT { get; set; }
+        public DateTime ToDT { get; set; }
+        public DateTime IC_ISSUED_DT { get; set; }
     }
 }
