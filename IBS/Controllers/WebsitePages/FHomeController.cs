32a957077d75920bb035630463b0703cfca32911
HardikGujjarSTTL <hardik.gujjar@silvertouch.com>
2023-09-04 18:05:30 +0530

    Add Frontend pages with css and js

---
+++ IBS/Controllers/WebsitePages/FHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBS.Controllers.WebsitePages
+{
+    public class FHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
