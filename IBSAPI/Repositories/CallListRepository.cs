0b853dfcd50733dc4b737fa9364f65f9bedbdd12
HardikGujjarSTTL <hardik.gujjar@silvertouch.com>
2023-10-31 16:08:43 +0530

    Create new API

--- IBSAPI/Repositories/CallListRepository.cs
+++ IBSAPI/Repositories/CallListRepository.cs
@@ -27,34 +27,5 @@ namespace IBSAPI.Repositories
             return lst;
         }
 
-        public List<CallRegiModel> GetCaseDetailsforvendor(int UserID)
-        {
-            List<CallRegiModel> lst = new();
-
-            lst = (from x in context.T17CallRegisters
-                  join y in context.T13PoMasters on x.CaseNo equals y.CaseNo
-                  join p in context.V06Consignees on y.PurchaserCd equals p.ConsigneeCd
-                   join v in context.T05Vendors on y.VendCd equals v.VendCd
-                   join cs in context.T21CallStatusCodes on x.CallStatus equals cs.CallStatusCd
-                   join ie in context.T09Ies on x.IeCd equals ie.IeCd
-                   where x.MfgCd == UserID
-                   select new CallRegiModel
-                   {
-                       CaseNo = x.CaseNo,
-                       CallDate = x.Datetime,
-                       CallSNo = x.CallSno,
-                       Purchaser = p.Consignee,
-                       Vendor = v.VendName,
-                       PurchaseOrderDate=y.PoDt,
-                       PurchaseOrderNo = y.PoNo,
-                       CallStatus = cs.CallStatusDesc,
-                       Region = x.RegionCode,
-                       PlaceofInspection = x.MfgPlace,
-                       ContactPersonName = ie.IeName,
-                       ManufacturerEmail = ie.IeEmail,
-                       PhoneNumber = ie.IePhoneNo,
-                   }).ToList();
-            return lst;
-        }
     }
 }
