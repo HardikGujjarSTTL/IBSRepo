2402378d8bb62390c511317ea6544ef18881abf4
Bhaveshrathod321 <bhavesh.rathod@silvertouch.com>
2023-11-03 18:08:14 +0530

    code commited

--- IBS/Models/Common.cs
+++ IBS/Models/Common.cs
@@ -2924,7 +2924,7 @@ namespace IBS.Models
         {
             ModelContext context = new(DbContextHelper.GetDbContextOptions());
             List<SelectListItem> Bank = (from a in context.T94Banks
-
+                                         orderby a.BankName
                                          select
                                     new SelectListItem
                                     {
