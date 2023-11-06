9380b72c1eff43bedfa316c1ac4cfa57b39f692d
HardikGujjarSTTL <hardik.gujjar@silvertouch.com>
2023-11-06 20:27:07 +0530

    API

---
+++ IBSAPI/Controllers/ClientController.cs
@@ -0,0 +1,56 @@
+﻿using IBSAPI.Interfaces;
+using IBSAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IBSAPI.Controllers
+{
+    public class ClientController : Controller
+    {
+        #region Variable
+        private readonly IVendorRepository vendorRepository;
+        #endregion
+
+        public ClientController(IVendorRepository _vendorRepository)
+        {
+            vendorRepository = _vendorRepository;
+        }
+
+        [HttpGet("GetCaseDetailsforClient", Name = "GetCaseDetailsforClient")]
+        public IActionResult GetCaseDetailsforClient(string UserID, string Organisation, string OrgnType)
+        {
+            try
+            {
+                List<CallRegiModel> callRegiModels = vendorRepository.GetCaseDetailsforClient(UserID, Organisation, OrgnType);
+                if (callRegiModels.Count() > 0)
+                {
+                    var response = new
+                    {
+                        resultFlag = (int)Helper.Enums.ResultFlag.SucessMessage,
+                        message = "Successfully",
+                        data = callRegiModels
+                    };
+                    return Ok(response);
+                }
+                else
+                {
+                    var response = new
+                    {
+                        resultFlag = (int)Helper.Enums.ResultFlag.ErrorMessage,
+                        message = "No Data Found",
+                    };
+                    return Ok(response);
+                }
+            }
+            catch (Exception ex)
+            {
+                Common.AddException(ex.ToString(), ex.Message.ToString(), "Vendor_API", "GetCaseDetailsforvendor", 1, string.Empty);
+                var response = new
+                {
+                    resultFlag = (int)Helper.Enums.ResultFlag.ErrorMessage,
+                    message = ex.Message.ToString(),
+                };
+                return Ok(response);
+            }
+        }
+    }
+}
--- IBSAPI/Repositories/VendorRepository.cs
+++ IBSAPI/Repositories/VendorRepository.cs
@@ -42,5 +42,36 @@ namespace IBSAPI.Repositories
                    }).ToList();
             return lst;
         }
+
+        public List<CallRegiModel> GetCaseDetailsforClient(string UserID, string Organisation, string OrgnType)
+        {
+            List<CallRegiModel> lst = new();
+            string threeMonthsAgo = DateTime.Now.AddMonths(-3).ToString("dd-MM-yy");
+
+            lst = (from x in context.T17CallRegisters
+                   join y in context.T13PoMasters on x.CaseNo equals y.CaseNo
+                   join p in context.V06Consignees on y.PurchaserCd equals p.ConsigneeCd
+                   join v in context.T05Vendors on y.VendCd equals v.VendCd
+                   join cs in context.T21CallStatusCodes on x.CallStatus equals cs.CallStatusCd
+                   join ie in context.T09Ies on x.IeCd equals ie.IeCd
+                   where y.RlyCd == Organisation && y.RlyNonrly == OrgnType && x.CallRecvDt >= Convert.ToDateTime(threeMonthsAgo)
+                   select new CallRegiModel
+                   {
+                       CaseNo = x.CaseNo,
+                       CallDate = x.Datetime,
+                       CallSNo = x.CallSno,
+                       Purchaser = p.Consignee,
+                       Vendor = v.VendName,
+                       PurchaseOrderDate = y.PoDt,
+                       PurchaseOrderNo = y.PoNo,
+                       CallStatus = cs.CallStatusDesc,
+                       Region = x.RegionCode,
+                       PlaceofInspection = x.MfgPlace,
+                       ContactPersonName = ie.IeName,
+                       ManufacturerEmail = ie.IeEmail,
+                       PhoneNumber = ie.IePhoneNo,
+                   }).ToList();
+            return lst;
+        }
     }
 }
