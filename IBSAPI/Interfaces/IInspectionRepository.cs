64e3197f08a2a602388dc1f88e92746d615a654e
naimishra <naimish.rana@silvertouch.com>
2023-11-08 14:47:54 +0530

    Commit Code

--- IBSAPI/Interfaces/IInspectionRepository.cs
+++ IBSAPI/Interfaces/IInspectionRepository.cs
@@ -4,7 +4,7 @@ namespace IBSAPI.Interfaces
 {
     public interface IInspectionRepository
     {
-        #region IE
+        #region IE Methods
         List<TodayInspectionModel> GetToDayInspection(int IeCd);
         List<TomorrowInspectionModel> GetTomorrowInspection(int IeCd);
         List<PendingInspectionModel> GetPendingInspection(int IeCd, string Region, DateTime CurrDate);
@@ -13,12 +13,17 @@ namespace IBSAPI.Interfaces
         List<CompleteInspectionModel> GetCompleteInspection(int IeCd);
         #endregion
 
-        #region Vendor
+        #region Vendor Methods
         List<VendorPedingInspectionModel> Get_Vendor_PendingInspection(int Vend_Cd, DateTime FromDate, DateTime ToDate);
         #endregion
 
-        #region CM
+        #region CM Methods
         List<RecentInspectionModel> Get_CM_RecentInspection(int CO_CD, DateTime CurrDate);
+        #endregion
+
+        #region Client Methods
+        List<PendingInspectionModel> Get_Client_PendingInspection(string Rly_CD, string Rly_NonType, DateTime FromDate, DateTime ToDate);
+        List<PendingInspectionModel> Get_Client_Region_Wise_PendingInspection(string Rly_CD, string Rly_NonType, string Region, DateTime FromDate, DateTime ToDate);
         #endregion
     }
 }
