ad99f8bd023a309ed7984df77ac08fce299ca75b
Bhaveshrathod321 <bhavesh.rathod@silvertouch.com>
2023-11-21 16:19:52 +0530

    code commited

--- IBS/Interfaces/IDashboardRepository.cs
+++ IBS/Interfaces/IDashboardRepository.cs
@@ -7,5 +7,11 @@ namespace IBS.Interfaces
         public DashboardModel GetIEDDashBoardCount(int IeCd);
         public DashboardModel GetVendorDashBoardCount(int Vend_Cd);
         public DashboardModel GetClientDashBoardCount(string OrgnType,string Organisation);
+
+        DTResult<VenderCallRegisterModel> GetDataListTotalCallListing(DTParameters dtParameters, string Region);
+
+        DTResult<VenderCallRegisterModel> GetDataCallDeskInfoListing(DTParameters dtParameters, string Region);
+
+        
     }
 }
