4bc3f5847bd8b0013a6649be2897c00fc5c1a9bc
23080102 <23080102@DESKTOP-FK9USVM>
2023-12-21 17:57:45 +0530

    payment gateway pdf

--- IBS/Models/OnlinePaymentGateway.cs
+++ IBS/Models/OnlinePaymentGateway.cs
@@ -28,11 +28,39 @@ namespace IBS.Models
         public string? Location { get; set; }
         public string? City { get; set; }
         public int? VEND_CD { get; set; }
+        [Required]
+        [Display(Name = "Charges")]
         public decimal? Charges { get; set; }
         public string MER_TXN_REF { get; set; }
+        [Required]
+        [Display(Name = "Charges Type")]
         public string ChargesType { get; set; }
         public int? ORDER_INFO { get; set; }
         public string Tok_id { get; set; }
         public string DATETIME { get; set; }
+        public string MerchantID { get; set; }
+        public string OrderInfo { get; set; }
+        public string TransactionAmount { get; set; }
+        public string TranRespCode { get; set; }
+        public string TranRespCodeDesc { get; set; }
+        public string PaymentServerMsg { get; set; }
+        public string AcquirerRespCode { get; set; }
+        public string ShoppingTranNO { get; set; }
+        public string ReceiptNo { get; set; }
+        public string AuthorizationID { get; set; }
+        public string BatchNo { get; set; }
+        public string CardType { get; set; }
+        public string CSCResultCode { get; set; }
+        public string CSCResultDescrip { get; set; }
+        public string ECI { get; set; }
+        public string XID { get; set; }
+        public string Enrolled { get; set; }
+        public string Status { get; set; }
+        public string VerToken { get; set; }
+        public string VerType { get; set; }
+        public string VerSecurityLevel { get; set; }
+        public string VerStatus { get; set; }
+        public string RiskOverallResult { get; set; }
+        public string TranReversalResult { get; set; }
     }
 }
