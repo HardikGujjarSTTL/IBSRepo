22ec7c95c53500010708f0e9f98fc8f52af559dc
vishaldarji09 <vishal.darji@silvertouch.com>
2024-01-03 10:38:03 +0530

    chat related changes

---
+++ IBS/Helper/ChatHub.cs
@@ -0,0 +1,42 @@
+﻿using IBS.Interfaces.Hub;
+using IBS.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace IBS.Helper
+{
+    public class ChatHub : Hub
+    {
+        private readonly IChatRepository _chathub;
+
+        public ChatHub(IChatRepository chathub)
+        {
+            _chathub = chathub;
+        }
+
+        public async Task OnConnectedAsync(string SenderId)
+        {
+            foreach (var item in Common.ConnectedUsers.Where(kvp => kvp.Value == SenderId).ToList())
+            {
+                Common.ConnectedUsers.Remove(item.Key);
+            }
+
+            Common.ConnectedUsers.Add(Context.ConnectionId, SenderId);
+        }
+
+        public async Task SendMessage(string SenderId, string ReceiverId, string message)
+        {
+            ChatMessage model = new ChatMessage();
+            model.msg_send_ID = Convert.ToInt32(SenderId);
+            model.msg_recv_ID = Convert.ToInt32(ReceiverId);
+            model.message = message;
+            var res = _chathub.ChatMessageSave(model);
+
+            var myKey = Common.ConnectedUsers.Where(x => x.Value == ReceiverId || x.Value == SenderId).ToList();
+
+            foreach (var item in myKey)
+            {
+                await Clients.Clients(item.Key).SendAsync("ReceiveMessage", ReceiverId, message);
+            }
+        }
+    }
+}
