ab1f626fe9bd0ed3e3cc222a6474d3eb3d089e54
naimishra <naimish.rana@silvertouch.com>
2024-01-11 17:28:33 +0530

    Chat Changes

---
+++ IBS/wwwroot/js/ChatMessageHistory.js
@@ -0,0 +1,24 @@
+﻿$('#txtMsg').keypress(function (e) {
+    if (e.keyCode == 13) {  // detect the enter key
+        SendMessage(2);
+    }
+});
+
+$('#fileupload2').change(function (e) {
+    handleFile('fileupload2');
+});
+
+$(".deleteSelect").click(function () {
+    $(".fileUploadDelete").slideToggle();
+});
+
+function CloseFileUploadPopup() {
+    $(".fileUploadedInfo").hide();
+
+    $("#hdnBase64String").val("");
+    $("#hdnFileName").val("");
+    $("#hdnFileSize").val("");
+
+    $("#FileSize").html("");
+    $(".fileUploadFileName").html("");
+}
