80e229620de3d60db002b5c58ae61ef7b9e821b2
naimishra <naimish.rana@silvertouch.com>
2024-01-16 18:28:31 +0530

    Chat changes Commit  code

--- IBS/Models/Common.cs
+++ IBS/Models/Common.cs
@@ -1206,7 +1206,7 @@ namespace IBS.Models
             textValueDropDownDTO.Add(single);
             return textValueDropDownDTO.ToList();
         }
-        
+
         public static List<SelectListItem> LOA()
         {
             List<SelectListItem> textValueDropDownDTO = new List<SelectListItem>();
@@ -4903,7 +4903,7 @@ namespace IBS.Models
         {
             ModelContext ModelContext = new(DbContextHelper.GetDbContextOptions());
             List<SelectListItem> UM = (from a in ModelContext.UserMasters
-                                       where a.UserType == UsertType || a.UserType == "IE"
+                                       where a.UserType == UsertType
                                        orderby a.UserType ascending, a.Name ascending
                                        select new SelectListItem
                                        {
@@ -4911,6 +4911,19 @@ namespace IBS.Models
                                            Value = Convert.ToString(a.Id)
                                        }).ToList();
             return UM;
+        }
+
+        public static List<SelectListItem> GetUserTypeForMaster()
+        {
+            List<SelectListItem> list = new List<SelectListItem>
+            {
+                new SelectListItem { Text = "Users", Value = "USERS" },
+                new SelectListItem { Text = "Vendor", Value = "VENDOR" },
+                new SelectListItem { Text = "Inspection Engineer (IE)", Value = "IE" },
+                new SelectListItem { Text = "Client", Value = "CLIENT_LOGIN" },
+                new SelectListItem { Text = "Liaisoning Officer (LO)", Value = "LO_LOGIN" },
+            };
+            return list;
         }
     }
 
