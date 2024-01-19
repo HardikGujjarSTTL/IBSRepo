7e2400cad9c0c50b94f9029eda474a92e3e7b81b
naimishra <naimish.rana@silvertouch.com>
2024-01-19 12:54:44 +0530

    Commit Code

--- IBS/Helper/ChatHub.cs
+++ IBS/Helper/ChatHub.cs
@@ -124,62 +124,6 @@ namespace IBS.Helper
             writer.Write(data);
         }
 
-        public async Task SendMessage1(string SenderId, string ReceiverId, string message, int MsgType, IFormFile? file)
-        {
-            string FileDisplayName = null, Field_ID = null, Extension = null;
-
-            ChatMessage model = new ChatMessage();
-            model.msg_send_ID = SenderId;
-            model.msg_recv_ID = ReceiverId;
-            model.message = message;
-            if (file != null)
-            {
-                Guid newGuid = Guid.NewGuid();
-                model.RelativePath = "../ReadWriteData/CHAT_FILES";
-                model.Field_ID = Path.GetFileNameWithoutExtension(file.FileName) + "_" + newGuid.ToString() + Path.GetExtension(file.FileName);
-                model.Extension = Path.GetExtension(file.FileName);
-                model.FileDisplayName = file.FileName;
-                FileDisplayName = file.FileName;
-                Field_ID = model.Field_ID;
-                Extension = Path.GetExtension(file.FileName);
-            }
-            var res = _chathub.ChatMessageSave(model);
-
-            if (file != null)
-            {
-                var path = _env.WebRootPath + "/ReadWriteData/CHAT_FILES";
-                path = Path.Combine(path, model.Field_ID);
-                using (var stream = new FileStream(path, FileMode.Create))
-                {
-                    file.CopyToAsync(stream);
-                }
-            }
-
-            var arrRecvId = string.IsNullOrEmpty(model.msg_recv_ID) ? new List<string>() : model.msg_recv_ID.Split(",").ToList();
-            if (arrRecvId.Count() > 0)
-            {
-                foreach (var recvID in arrRecvId)
-                {
-                    var myKey = Common.ConnectedUsers.Where(x => x.Value == recvID || x.Value == SenderId).ToList();
-
-                    foreach (var k in myKey)
-                    {
-                        var lstChat = _chathub.GetMessageList(Convert.ToInt32(SenderId), Convert.ToInt32(recvID)).lstMsg;
-                        var CurrDateCount = lstChat.Where(x => x.Msg_Date.Date == DateTime.Now.Date).Count();
-                        //model.CurrDateCount = model.lstMsg.Where(x => x.Msg_Date.Date == DateTime.Now.Date).Count();
-                        await Clients.Clients(k.Key).SendAsync("ReceiveMessage", recvID, message, MsgType, Field_ID, FileDisplayName, Extension, CurrDateCount);
-                    }
-                }
-            }
-            #region Old Response
-            //var myKey = Common.ConnectedUsers.Where(x => x.Value == ReceiverId || x.Value == SenderId).ToList();
-            //foreach (var item in myKey)
-            //{
-            //    await Clients.Clients(item.Key).SendAsync("ReceiveMessage", ReceiverId, message, MsgType);                
-            //}
-            #endregion
-        }
-
         public async Task DeleteMessage(string SenderId, string ReceiverId, int DeletedId)
         {
             string DeleteDate = "", DeleteFileName = "";
