d5070e0a81f384e025288e64bf31a0e73442c81f
Bhaveshrathod321 <bhavesh.rathod@silvertouch.com>
2024-02-06 11:59:39 +0530

    code commited

--- IBS/Repositories/InspectionBilling/CallRegisterIBRepository.cs
+++ IBS/Repositories/InspectionBilling/CallRegisterIBRepository.cs
@@ -5334,7 +5334,7 @@ namespace IBS.Repositories.InspectionBilling
                 model.AlertMsg = "Your Call Status is Blank, Kindly Goto Mainmenu and select the call again to update!!!";
                 return model;
             }
-            else if (model.CallStatus.Trim() == "R" && model.RejectionCharge == "" || model.RejectionCharge == null)
+            else if (model.CallStatus.Trim() == "R" && model.RejectionCharge == null)
             {
                 model.AlertMsg = "Kindly Enter Rejection Charges in Case of Rejection IC!!!";
                 return model;
