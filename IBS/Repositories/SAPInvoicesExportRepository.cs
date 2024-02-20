8ed1f18b88579a6fb8ba9865f75e17353d1921ed
Bhaveshrathod321 <bhavesh.rathod@silvertouch.com>
2024-02-20 19:03:03 +0530

    code commited

---
+++ IBS/Repositories/SAPInvoicesExportRepository.cs
@@ -0,0 +1,15 @@
+﻿using IBS.DataAccess;
+using IBS.Interfaces;
+
+namespace IBS.Repositories
+{
+    public class SAPInvoicesExportRepository : ISAPInvoicesExportRepository
+    {
+        private readonly ModelContext context;
+
+        public SAPInvoicesExportRepository(ModelContext context)
+        {
+            this.context = context;
+        }
+    }
+}
